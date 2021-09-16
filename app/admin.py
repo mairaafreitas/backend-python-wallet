87bf06bcd2b07a66e8c998c789b132e33bdda587
Maira Freitas <maira.oliveirafreitas@gmail.com>
2021-09-16 17:07:32 -0300

    register models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import Cashback, Customer, Products
 
-# Register your models here.
+
+class ManageCashback(admin.ModelAdmin):
+    list_display = ('id', 'sold_at', 'customer', 'total')
+
+
+admin.site.register(Cashback, ManageCashback)
+admin.site.register(Customer)
+admin.site.register(Products)
--- app/models.py
+++ app/models.py
@@ -14,6 +14,6 @@ class Products(models.Model):
 
 class Cashback(models.Model):
     sold_at = models.DateTimeField(auto_now_add=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=15, decimal_places=2)
-    products = models.ManyToManyField(Products)
+    products = models.ManyToManyField('Products')
