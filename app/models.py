7ad0c99e4e95cb2b51bac89cbd65ab9411a9c347
Maira Freitas <maira.oliveirafreitas@gmail.com>
2021-09-16 16:16:53 -0300

    create models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    document = models.PositiveBigIntegerField()
+    name = models.CharField(max_length=80, blank=False)
+
+
+class Products(models.Model):
+    type = models.CharField(max_length=10)
+    value = models.DecimalField(max_digits=15, decimal_places=2)
+    qty = models.PositiveIntegerField()
+
+
+class Cashback(models.Model):
+    sold_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=15, decimal_places=2)
+    products = models.ManyToManyField(Products)
