80e92c3c047e83991c08efb59d259e7cc098ea95
Maira Freitas <maira.oliveirafreitas@gmail.com>
2021-09-18 23:18:39 -0300

    override post method to calculate cashback

--- app/models.py
+++ app/models.py
@@ -3,27 +3,33 @@ from django.core.validators import MinLengthValidator, MinValueValidator
 
 
 class Customer(models.Model):
+    """Database model for product in the system"""
     document = models.CharField(max_length=11, validators=[MinLengthValidator(11)])
     name = models.CharField(max_length=80, blank=False)
 
     def __str__(self):
+        """Return string representation of customer"""
         return self.name
 
 
 class Product(models.Model):
+    """Database model for customer in the system"""
     type = models.CharField(max_length=10)
     value = models.DecimalField(max_digits=15, decimal_places=2)
     qty = models.PositiveIntegerField()
 
     def __str__(self):
+        """Return string representation of product"""
         return self.type
 
 
 class Cashback(models.Model):
+    """Database model for cashback in the system"""
     sold_at = models.DateTimeField()
     customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
     total = models.DecimalField(max_digits=15, decimal_places=2, validators=[MinValueValidator(0)])
     products = models.ManyToManyField('Product')
 
     def __str__(self):
+        """Return string representation of cashback and id"""
         return f"Cashback #{self.id}"
--- app/views.py
+++ app/views.py
@@ -1,4 +1,8 @@
-from rest_framework import viewsets
+from decimal import Decimal
+
+from rest_framework import viewsets, views
+from rest_framework.response import Response
+
 from app.models import Cashback
 from app.serializer import CashbackSerializer
 
@@ -6,3 +10,20 @@ from app.serializer import CashbackSerializer
 class CashbackViewSet(viewsets.ModelViewSet):
     queryset = Cashback.objects.all()
     serializer_class = CashbackSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = CashbackSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        data = serializer.validated_data
+        total_data = data['total']
+        print(total_data)
+
+        cashback_calculation = total_data * Decimal('0.10')
+        print(cashback_calculation)
+
+        serializer.create(data)
+
+        return Response({f"cashback_calculation: {cashback_calculation}"})
+
+
